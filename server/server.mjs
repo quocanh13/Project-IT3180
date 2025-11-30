c378c138e159c404578d25598fb2779f56114d52
quocanh13 <nguyenvan2308199@gmail.com>
2025-11-30 23:15:06 +0700

    Tạo server

--- server/server.mjs
+++ server/server.mjs
@@ -1,6 +1,9 @@
-<<<<<<< HEAD
-123
-=======
-//abcd3
+import express from "express"
 
->>>>>>> 5942c06a286d639f311bc7eed38d9fb69cf4e974
+const server = express();
+
+server.use(express.static("public"));
+
+server.listen(80, "::", ()=>{
+    console.log("Server is listening");
+})
