6e20ce0a6fae51f45bd4fbd3618039f510ef0e41
quocanh13 <nguyenvan2308199@gmail.com>
2025-11-30 21:31:55 +0700

    Cập nhật chú thích cho ServerResponse trong data-types.mjs

--- server/public/utils/data-types.mjs
+++ server/public/utils/data-types.mjs
@@ -1,8 +1,37 @@
 export class ServerResponse{
-    /**@type {"OK" | "ERROR" | "REDIRECT" | "BAD REQUEST"} */
+    /**
+     * @type {"OK" | "ERROR" | "REDIRECT" | "BAD REQUEST"} 
+     * 
+     * Kiểu của phản hồi 
+     * 
+     * OK - Request đã được xử lý thành công \n
+     * 
+     * ERROR - Có lỗi nào đó ở server
+     * 
+     * REDIRECT - Yêu cầu client điều hướng theo redirectURL
+     * 
+     * BAD REQUEST - Thông tin mà client gửi lên server không hợp lệ
+     */
     type;
-    /**@type {string} */
+
+    /**
+     * @type {string} 
+     * 
+     * Thông điệp server gửi về cho client để thông báo về kết quả của request
+     */
     message;
-    /**@type {*} */
+    
+    /**
+     * @type {*} 
+     * 
+     * Dữ liệu đi kèm nếu type là OK khi người dùng cần lấy dữ liệu 
+     */
     data;
+
+    /**
+     * @type {string} 
+     * 
+     * URL để điều hướng khi type = REDIRECT
+     */
+    redirectURl;
 }
