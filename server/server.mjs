d670af27bae7e80bff64142386033f791533264a
quocanh13 <nguyenvan2308199@gmail.com>
2025-12-01 10:21:12 +0700

    login-handler

--- server/server.mjs
+++ server/server.mjs
@@ -5,7 +5,7 @@ import staticRouter from "./routers/static-router.mjs"
 const server = express();
 
 server.use(express.static("public"));
-
+server.use(express.json());
 server.use([staticRouter]);
 
 server.listen(80, "::", ()=>{
--- server/services/user/user.mjs
+++ server/services/user/user.mjs
@@ -3,7 +3,7 @@
  * @param {string} password 
  * @returns {Promise<"OK" | "TÊN ĐĂNG NHẬP KHÔNG TỒN TẠI" | "MẬT KHẨU KHÔNG ĐÚNG" | "KHÔNG ĐÚNG ĐỊNH DẠNG" | "SERVER CÓ LỖI">}
  */
-async function login(username, password) {
+export async function login(username, password) {
     
 }
 
