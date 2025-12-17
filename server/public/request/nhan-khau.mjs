84d47378a06d044606f401cfbfc42d9baa2c08d6
quocanh13 <nguyenvan2308199@gmail.com>
2025-12-17 19:44:38 +0700

    Hàm getNhanKhau

---
+++ server/public/request/nhan-khau.mjs
@@ -0,0 +1,19 @@
+import { ServerResponse } from "../utils/data-types.mjs";
+
+/**
+ * Hàm lấy danh sách hộ khẩu 
+ * @param {number} offset - Vị trí bắt đầu để lấy danh sách hộ khẩu - Mặc định là 0
+ * @param {number} limit - Số lượng hộ khẩu được lấy, nếu là -1 thì lấy toàn bộ - Mặc định là 20
+ * @returns {ServerResponse}
+ */
+export async function getNhanKhau(offset = 0, limit = 20) {
+
+    const res = await fetch(
+        `/nhan_khau?offset=${offset}&limit=${limit}`,
+        {
+            method : "GET"
+        }
+    );
+
+    return await res.json();
+}
