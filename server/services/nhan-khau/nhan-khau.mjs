256d7c770e4f82ae4965c57b9d549c9e9978a5ff
quocanh13 <nguyenvan2308199@gmail.com>
2025-12-17 20:45:28 +0700

    Hàm getHoKhau để lấy thông tin hộ khẩu từ database

---
+++ server/services/nhan-khau/nhan-khau.mjs
@@ -0,0 +1,11 @@
+import { HoKhau } from "../../public/utils/data-types.mjs";
+
+/**
+ * Hàm lấy thông tin nhân khẩu
+ * @param {number} offset - Vị trí bắt đầu 
+ * @param {number} limit - Số lượng, nếu là -1 thì lấy toàn bộ nhân khẩu
+ * @returns {HoKhau[]}
+ */
+export async function getHoKhau(offset, limit) {
+    
+}
