83a6949eb6fdc153347e2f7357fa96bab338f196
CuongPhung <cuongdac24022005@gmail.com>
2025-12-23 07:45:51 +0700

    change login

--- server/handlers/static-handler.mjs
+++ server/handlers/static-handler.mjs
@@ -1,3 +1,7 @@
+export async function getLanding(req, res) {
+    res.redirect("/index.html");
+}
+
 export async function getLogin(req, res) {
     res.redirect("/login/login.html");
 }
--- server/routers/static-router.mjs
+++ server/routers/static-router.mjs
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getLogin } from "../handlers/static-handler.mjs";
+import {getLanding, getLogin } from "../handlers/static-handler.mjs";
 
 const router = Router();
 
-router.get(["/", "/login"], getLogin);
+router.get("/",getLanding);
+router.get("/login", getLogin);
 
 export default router;
